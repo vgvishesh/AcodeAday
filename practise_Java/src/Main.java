becaaadf6a514855e7baf7f87a8df7e058c2a3a9
vgvishesh2008 <vgvishesh2008@gmail.com>
2018-09-30 20:16:12 +0530

    Rust hello world

--- practise_Java/src/Main.java
+++ practise_Java/src/Main.java
@@ -15,5 +15,53 @@ public class Main {
         split.setMatrix(mat);
         //split.CreateBill();
         split.simplify();
+        int A = Integer.MAX_VALUE;
     }
 }
+
+/*
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int firstInitInput = in.nextInt();
+        int secondInitInput = in.nextInt();
+        int thirdInitInput = in.nextInt();
+        if (in.hasNextLine()) {
+            in.nextLine();
+        }
+
+        // game loop
+        while (true) {
+            String firstInput = in.nextLine();
+            String secondInput = in.nextLine();
+            String thirdInput = in.nextLine();
+            String fourthInput = in.nextLine();
+            for (int i = 0; i < thirdInitInput; i++) {
+                int fifthInput = in.nextInt();
+                int sixthInput = in.nextInt();
+            }
+            in.nextLine();
+
+            // Write an action using System.out.println()
+            // To debug: System.err.println("Debug messages...");
+
+            System.out.println("A");
+        }
+    }
+}
+
+2	4
+5	4
+10	10
+11	11
+3	12
+ */
